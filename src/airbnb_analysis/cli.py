9409cef1492594a9e5f1c2edaaacb107dd674a12
MAPALO LUKASHI <mlukashi@mail.yu.edu>
2023-12-05 18:51:18 -0500

    Update cli.py

--- src/airbnb_analysis/cli.py
+++ src/airbnb_analysis/cli.py
@@ -10,7 +10,7 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description="Airbnb Data Analysis.")
     # Set the default data path to the data/listings.csv relative to the script location
     default_data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'listings.csv')
-    parser.add_argument("--data-path", type=str, default=default_data_path, help="Path to the Airbnb dataset CSV file.")
+    parser.add_argument("--data-path", type=str, required=True, help="Path to the Airbnb dataset CSV file.")
     parser.add_argument("--action", type=str, choices=['summary', 'eda', 'inference'], required=True, help="The type of analysis to perform: summary, eda (exploratory data analysis), or inference.")
     parser.add_argument("--column", type=str, help="The column to analyze (required for some actions).")
     args = parser.parse_args(argv)
